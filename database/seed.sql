323e079824ce6b0cd398c98ea6b5cf0c1bc879c7
Triston Golden <triston_golden@yahoo.com>
2022-02-10 22:24:51 -0500

    seeds

---
+++ database/seed.sql
@@ -0,0 +1,34 @@
+USE employees;
+
+INSERT INTO department
+
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
+
+INSERT INTO role
+    (title, salary, department_id)
+
+VALUES
+    ('Sales Lead', 110000, 1),
+    ('Salesperson', 85000, 1),
+    ('Lead Engineer', 165000, 2),
+    ('Software Engineer', 125000, 2),
+    ('Account Manager', 175000, 3),
+    ('Accountant', 135000, 3),
+    ('Legal Team Lead', 275000, 4),
+    ('Lawyer', 210000, 4);
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Triston', 'Golden', 1, NULL),
+    ('James', 'Cooksey', 2, 1),
+    ('Quesi', 'Cato', 3, NULL),
+    ('Sim', 'Springer', 4, 3),
+    ('Chawn', 'Golden', 5, NULL),
+    ('Mark', 'Coulibaly', 6, 5),
+    ('Anthony', 'Edwards', 7, NULL),
+    ('Montel', 'Quinones', 8, 7);
--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function mainPrompts() {
           value: "UPDATE_EMPLOYEE_ROLE",
         },
         {
-          name: "View All of the Roles",
+          name: "View all of the Roles",
           value: "VIEW_ROLES",
         },
         {
@@ -62,7 +62,6 @@ function mainPrompts() {
       case "UPDATE_EMPLOYEE_ROLE":
         updateEmployeeRole();
         break;
-
       case "VIEW_DEPARTMENTS":
         viewDepartments();
         break;
@@ -79,4 +78,198 @@ function mainPrompts() {
         quit();
     }
   });
+}
+// View all of the employees
+function viewEmployees() {
+  db.findAllEmployees()
+    .then(([rows]) => {
+      let employees = rows;
+      console.log("\n");
+      console.table(employees);
+    })
+    .then(() => mainPrompts());
+}
+
+// Add an employee
+function addEmployee() {
+  prompt([
+    {
+      name: "first_name",
+      message: "What is the first name of the employee?",
+    },
+    {
+      name: "last_name",
+      message: "What is the last name of the employee?",
+    },
+  ]).then((res) => {
+    let firstName = res.first_name;
+    let lastName = res.last_name;
+
+    db.findAllRoles().then(([rows]) => {
+      let roles = rows;
+      const roleChoices = roles.map(({ id, title }) => ({
+        name: title,
+        value: id,
+      }));
+
+      prompt({
+        type: "list",
+        name: "roleId",
+        message: "What is the role of the employee?",
+        choices: roleChoices,
+      }).then((res) => {
+        let roleId = res.roleId;
+
+        db.findAllEmployees().then(([rows]) => {
+          let employees = rows;
+          const managerChoices = employees.map(
+            ({ id, first_name, last_name }) => ({
+              name: `${first_name} ${last_name}`,
+              value: id,
+            })
+          );
+
+          managerChoices.unshift({ name: "None", value: null });
+
+          prompt({
+            type: "list",
+            name: "managerId",
+            message: "Who is the manager of the employee?",
+            choices: managerChoices,
+          })
+            .then((res) => {
+              let employee = {
+                manager_id: res.managerId,
+                role_id: roleId,
+                first_name: firstName,
+                last_name: lastName,
+              };
+
+              db.createEmployee(employee);
+            })
+            .then(() =>
+              console.log(`Added ${firstName} ${lastName} to the database`)
+            )
+            .then(() => mainPrompts());
+        });
+      });
+    });
+  });
+}
+
+// Update the role of an employee
+function updateEmployeeRole() {
+  db.findAllEmployees().then(([rows]) => {
+    let employees = rows;
+    const employeeChoices = employees.map(({ id, first_name, last_name }) => ({
+      name: `${first_name} ${last_name}`,
+      value: id,
+    }));
+
+    prompt([
+      {
+        type: "list",
+        name: "employeeId",
+        message: "Which employee's role do you want to update?",
+        choices: employeeChoices,
+      },
+    ]).then((res) => {
+      let employeeId = res.employeeId;
+      db.findAllRoles().then(([rows]) => {
+        let roles = rows;
+        const roleChoices = roles.map(({ id, title }) => ({
+          name: title,
+          value: id,
+        }));
+
+        prompt([
+          {
+            type: "list",
+            name: "roleId",
+            message:
+              "Which role would you like to assign the current employee?",
+            choices: roleChoices,
+          },
+        ])
+          .then((res) => db.updateEmployeeRole(employeeId, res.roleId))
+          .then(() => console.log("Updated employee's role"))
+          .then(() => mainPrompts());
+      });
+    });
+  });
+}
+
+// View all of the roles
+function viewRoles() {
+  db.findAllRoles()
+    .then(([rows]) => {
+      let roles = rows;
+      console.log("\n");
+      console.table(roles);
+    })
+    .then(() => mainPrompts());
+}
+
+// Add a new role
+function addRole() {
+  db.findAllDepartments().then(([rows]) => {
+    let departments = rows;
+    const departmentChoices = departments.map(({ id, name }) => ({
+      name: name,
+      value: id,
+    }));
+
+    prompt([
+      {
+        name: "title",
+        message: "What is the name of the role?",
+      },
+      {
+        name: "salary",
+        message: "What is the salary of the role?",
+      },
+      {
+        type: "list",
+        name: "department_id",
+        message: "Which department does the role belong to?",
+        choices: departmentChoices,
+      },
+    ]).then((role) => {
+      db.createRole(role)
+        .then(() => console.log(`Added ${role.title} to the database`))
+        .then(() => mainPrompts());
+    });
+  });
+}
+
+// View all of the departments
+function viewDepartments() {
+  db.findAllDepartments()
+    .then(([rows]) => {
+      let departments = rows;
+      console.log("\n");
+      console.table(departments);
+    })
+    .then(() => mainPrompts());
+}
+
+// Add a new department
+function addDepartment() {
+  prompt([
+    {
+      name: "name",
+      message: "What is the name of the department?",
+    },
+  ]).then((res) => {
+    let name = res;
+    db.createDepartment(name)
+      .then(() => console.log(`Added ${name.name} to the database`))
+      .then(() => mainPrompts());
+  });
+}
+
+// Exit
+function quit() {
+  console.log("Farewell!");
+  process.exit();
 }
