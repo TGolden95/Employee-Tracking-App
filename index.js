01ecb51beae3a062347d94a1a661d241eb493798
Triston Golden <triston_golden@yahoo.com>
2022-02-10 20:39:32 -0500

    creating function

--- index.js
+++ index.js
@@ -1,11 +1,82 @@
 const inquirer = require("inquirer");
+const logo = require("asciiart-logo");
+const db = require("./db");
+const { inherits } = require("util");
 
-const questions = [
+init();
+
+function init() {
+  const text = logo({ name: "Employee Manager" }).render();
+}
+
+function mainPrompts() {
+  prompt([
     {
-        type: "input",
-        message: "What would you like to do?",
-        name: "Title",
-    }
-]
+      type: "list",
+      name: "choice",
+      message: "What would you like to do?",
+      choices: [
+        {
+          name: "View all of the Employees",
+          value: "VIEW_EMPLOYEES",
+        },
+        {
+          name: "Add an Employee",
+          value: "ADD_EMPLOYEE",
+        },
+        {
+          name: "Update the Role of an Employee",
+          value: "UPDATE_EMPLOYEE_ROLE",
+        },
+        {
+          name: "View All of the Roles",
+          value: "VIEW_ROLES",
+        },
+        {
+          name: "Add a new Role",
+          value: "ADD_ROLE",
+        },
+        {
+          name: "View all of the Departments",
+          value: "VIEW_DEPARTMENTS",
+        },
+        {
+          name: "Add a new Department",
+          value: "ADD_DEPARTMENT",
+        },
+        {
+          name: "Quit",
+          value: "QUIT",
+        },
+      ],
+    },
+  ]).then((res) => {
+    let choice = res.choice;
+    switch (choice) {
+      case "VIEW_EMPLOYEES":
+        viewEmployees();
+        break;
+      case "ADD_EMPLOYEE":
+        addEmployee();
+        break;
+      case "UPDATE_EMPLOYEE_ROLE":
+        updateEmployeeRole();
+        break;
 
-function mainPrompts()
+      case "VIEW_DEPARTMENTS":
+        viewDepartments();
+        break;
+      case "ADD_DEPARTMENT":
+        addDepartment();
+        break;
+      case "VIEW_ROLES":
+        viewRoles();
+        break;
+      case "ADD_ROLE":
+        addRole();
+        break;
+      default:
+        quit();
+    }
+  });
+}
