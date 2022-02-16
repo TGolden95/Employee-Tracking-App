290206f66d06512d95d0d6f79cbe291b4fd27069
Triston Golden <triston_golden@yahoo.com>
2022-02-15 21:52:15 -0500

    done

--- db/seed.sql
+++ db/seed.sql
@@ -1,7 +1,7 @@
 USE employees;
 
 INSERT INTO department
-
+    (name)
 VALUES
     ('Sales'),
     ('Engineering'),
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const inquirer = require("inquirer");
-const logo = require("asciiart-logo");
+const { prompt } = require("inquirer");
 const db = require("./db");
 const { inherits } = require("util");
+require("console.table");
 
 init();
 
 function init() {
-  const text = logo({ name: "Employee Manager" }).render();
+  mainPrompts();
 }
 
 function mainPrompts() {
@@ -17,32 +17,32 @@ function mainPrompts() {
       message: "What would you like to do?",
       choices: [
         {
-          name: "View all of the Employees",
-          value: "VIEW_EMPLOYEES",
-        },
-        {
-          name: "Add an Employee",
-          value: "ADD_EMPLOYEE",
-        },
-        {
-          name: "Update the Role of an Employee",
-          value: "UPDATE_EMPLOYEE_ROLE",
+          name: "View all of the Departments",
+          value: "VIEW_DEPARTMENTS",
         },
         {
           name: "View all of the Roles",
           value: "VIEW_ROLES",
         },
         {
-          name: "Add a new Role",
-          value: "ADD_ROLE",
-        },
-        {
-          name: "View all of the Departments",
-          value: "VIEW_DEPARTMENTS",
+          name: "View all of the Employees",
+          value: "VIEW_EMPLOYEES",
         },
         {
           name: "Add a new Department",
           value: "ADD_DEPARTMENT",
+        },
+        {
+          name: "Add a new Role",
+          value: "ADD_ROLE",
+        },
+        {
+          name: "Add a new Employee",
+          value: "ADD_EMPLOYEE",
+        },
+        {
+          name: "Update the Role of an Employee",
+          value: "UPDATE_EMPLOYEE_ROLE",
         },
         {
           name: "Quit",
@@ -53,26 +53,26 @@ function mainPrompts() {
   ]).then((res) => {
     let choice = res.choice;
     switch (choice) {
+      case "VIEW_DEPARTMENTS":
+        viewDepartments();
+        break;
+      case "VIEW_ROLES":
+        viewRoles();
+        break;
       case "VIEW_EMPLOYEES":
         viewEmployees();
+        break;
+      case "ADD_DEPARTMENT":
+        addDepartment();
+        break;
+      case "ADD_ROLE":
+        addRole();
         break;
       case "ADD_EMPLOYEE":
         addEmployee();
         break;
       case "UPDATE_EMPLOYEE_ROLE":
         updateEmployeeRole();
-        break;
-      case "VIEW_DEPARTMENTS":
-        viewDepartments();
-        break;
-      case "ADD_DEPARTMENT":
-        addDepartment();
-        break;
-      case "VIEW_ROLES":
-        viewRoles();
-        break;
-      case "ADD_ROLE":
-        addRole();
         break;
       default:
         quit();
